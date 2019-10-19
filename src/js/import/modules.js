5a4781b962b3440e700c33c5257b13b217f637d9
Elena <elena.do1988@yandex.ru>
2019-10-19 15:59:22 +0300

    dropdown, date dropdown, filter date dropdown are added

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,5 @@
 import "%modules%/input-date-mask/input-date-mask";
 import "%modules%/btn-like/btn-like";
 import "%modules%/btn-rate/btn-rate";
+import "%modules%/dropdown/dropdown";
+import "%modules%/date-dropdown/date-dropdown";
