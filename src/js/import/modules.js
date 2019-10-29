636fcefa37b68412121c82fbb71a241a140b6fc9
Elena <elena.do1988@yandex.ru>
2019-10-29 13:43:57 +0300

    pagination is added

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/input-date-mask/input-date-mask";
 import "%modules%/btn-like/btn-like";
 import "%modules%/btn-rate/btn-rate";
+import "%modules%/pagination/pagination";
 import "%modules%/range-slider/range-slider";
